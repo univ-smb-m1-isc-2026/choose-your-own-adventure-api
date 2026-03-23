8c9aebc9c5f8430c8ef6da5364c96a3cae265e2e
jassem <jassem@slackintosh.dd>
2026-03-23 12:30:03 +0100

    dockerfile

---
+++ src/main/java/com/cyoa/api/CyoaApiApplication.java
@@ -0,0 +1,13 @@
+package com.cyoa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyoaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyoaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyoa/api/CyoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyoa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyoaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
